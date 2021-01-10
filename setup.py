cfed49de014179c7e937b2335592f0aeb84ac0ee
wassname <wassname@users.noreply.github.com>
2021-01-10 08:56:59 +0800

    no boto

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='gym_recording-modified',
       version='0.0.1',
-      install_requires=['gym', 'boto3'],
+      install_requires=['gym'],
       packages=find_packages(),
 )
